1eb3151c82a0d884849921e345497a4169962757
Tshireletso11 <133275866+Tshireletso11@users.noreply.github.com>
2023-08-23 17:38:03 +0200

    Update exits.c

--- exits.c
+++ exits.c
@@ -8,24 +8,25 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-int f, t;
-char *s = dest;
-f = 0;
-while (src[f] != '\0' && f < n - 1)
-{
-dest[f] = src[f];
-f++;
-{
-if (f < n)
-{
-t = f;
-while (t < n)
-{
-dest[t] = '\0';
-t++;
-}
-}
-return (s);
+	int f, t;
+	char *s = dest;
+
+	f = 0;
+	while (src[f] != '\0' && f < n - 1)
+	{
+		dest[f] = src[f];
+		f++;
+	}
+	if (f < n)
+	{
+		t = f;
+		while (t < n)
+		{
+			dest[t] = '\0';
+			t++;
+		}
+	}
+	return (s);
 }
 /**
 **_strncat - focuses two strings
@@ -36,22 +37,24 @@ return (s);
 */
 char *_strncat(char *dest, char *src, int n)
 {
-int f, t;
-char *s = dest;
-f = 0;
-t = 0;
-while (dest[f] != '\0')
-f++;
-while (src[t] != '\0' && t < n)
-{
-dest[f] = src[t];
-f++;
-t++;
+	int f, t;
+	char *s = dest;
+
+	f = 0;
+	t = 0;
+	while (dest[f] != '\0')
+		f++;
+	while (src[t] != '\0' && t < n)
+	{
+		dest[f] = src[f];
+		f++;
+		t++;
+	}
+	if (t < n)
+		dest[f] = '\0';
+	return (s);
 }
-if (t < n)
-dest[f] = '\0';
-return (s);
-}
+
 /**
 **_strchr - locates a character in a string
 *@s: the string to be parsed
@@ -60,9 +63,10 @@ return (s);
 */
 char *_strchr(char *s, char c)
 {
-do {
-if (*s == c)
-return (s);
-} while (*s++ != '\0');
-return (NULL);
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
 }
