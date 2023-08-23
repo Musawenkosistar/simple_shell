c3c68bf49b8ffe0b4a7de857fd21a7076ea64b8c
Tshireletso11 <133275866+Tshireletso11@users.noreply.github.com>
2023-08-23 18:09:25 +0200

    Update getLine.c

--- getLine.c
+++ getLine.c
@@ -10,35 +10,36 @@
 */
 ssize_t input_buf(info_t *info, char **buf, size_t *len)
 {
-ssize_t v = 0;
-size_t len_p = 0;
-if (!*len) 
-{
-free(*buf);
-*buf = NULL;
-signal(SIGINT, sigintHandler);
+	ssize_t x = 0;
+	size_t len_p = 0;
+
+	if (!*len) 
+	{
+		free(*buf);
+		*buf = NULL;
+		signal(SIGINT, sigintHandler);
 #if USE_GETLINE
-v = getline(buf, &len_p, stdin);
+		x = getline(buf, &len_p, stdin);
 #else
-v = _getline(info, buf, &len_p);
+		x = _getline(info, buf, &len_p);
 #endif
-if (v > 0)
-{
-if ((*buf)[v - 1] == '\n')
-{
-(*buf)[v - 1] = '\0'; 
-v--;
-}
-info->linecount_flag = 1;
-remove_comments(*buf);
-build_history_list(info, *buf, info->histcount++);
-{
-*len = v;
-info->cmd_buf = buf;
-}
-}
-}
-return (v);
+		if <x > 0)
+		{
+			if ((*buf)[x - 1] == '\n')
+			{
+				(*buf)[x - 1] = '\0'; 
+				x--;
+			}
+			info->linecount_flag = 1;
+			remove_comments(*buf);
+			build_history_list(info, *buf, info->histcount++);
+			{
+				*len = x;
+				info->cmd_buf = buf;
+			}
+		}
+	}
+	return (x);
 }
 /**
 * get_input - gets a line minus the newline
@@ -48,54 +49,61 @@ return (v);
 */
 ssize_t get_input(info_t *info)
 {
-static char *buf; 
-static size_t x, f, len;
-ssize_t v = 0;
-char **buf_p = &(info->arg), *p;
-_putchar(BUF_FLUSH);
-v = input_buf(info, &buf, &len);
-if (f == -1) 
-return (-1);
-if (len) 
-{
-f = x; 
-p = buf + x; 
-check_chain(info, buf, &f, x, len);
-while (f < len) 
-{
-if (is_chain(info, buf, &f))
-break;
-f++;
+	static char *buf; 
+	static size_t f, j, len;
+	ssize_t x = 0;
+	char **buf_p = &(info->arg), *p;
+
+	_putchar(BUF_FLUSH);
+	x = input_buf(info, &buf, &len);
+	if (x == -1) /* EOF */
+		return (-1);
+	if (len)	
+	{
+		j = f; 
+		p = buf + f;
+
+		check_chain(info, buf, &j, f, len);
+		while (j < len) 
+		{
+			if (is_chain(info, buf, &j))
+				break;
+			j++;
+		}
+
+		f = j + 1; 
+		if (f >= len) 
+		{
+			f = len = 0; 
+			info->cmd_buf_type = CMD_NORM;
+		}
+
+		*buf_p = p; 
+		return (_strlen(p)); 
+	}
+
+	*buf_p = buf; 
+	return (x); 
 }
-x = f + 1; 
-if (x >= len) 
-{
-x = len = 0;
-info->cmd_buf_type = CMD_NORM;
-}
-*buf_p = p; 
-return (_strlen(p)); 
-}
-*buf_p = buf; 
-return (v); 
-}
+
 /**
 * read_buf - reads a buffer
 * @info: parameter struct
 * @buf: buffer
 * @i: size
 *
-* Return: v
+* Return: x
 */
 ssize_t read_buf(info_t *info, char *buf, size_t *i)
 {
-ssize_t v = 0;
-if (*i)
-return (0);
-v = read(info->readfd, buf, READ_BUF_SIZE);
-if (v >= 0)
-*i = v;
-return (v);
+	ssize_t x = 0;
+
+	if (*f)
+		return (0);
+	x = read(info->readfd, buf, READ_BUF_SIZE);
+	if (x >= 0)
+		*f = x;
+	return (x);
 }
 /**
 * _getline - gets the next line of input from STDIN
@@ -107,45 +115,52 @@ return (v);
 */
 int _getline(info_t *info, char **ptr, size_t *length)
 {
-static char buf[READ_BUF_SIZE];
-static size_t x, len;
-size_t k;
-ssize_t v = 0, y = 0;
-char *p = NULL, *new_p = NULL, *c;
-p = *ptr;
-if (p && length)
-y = *length;
-if (x == len)
-x = len = 0;
-v = read_buf(info, buf, &len);
-if (v == -1 || (v == 0 && len == 0))
-return (-1);
-c = _strchr(buf + v, '\n');
-k = c ? 1 + (unsigned int)(c - buf) : len;
-new_p = _realloc(p, y, y ? y + k : k + 1);
-if (!new_p) 
-return (p ? free(p), -1 : -1);
-if (y)
-_strncat(new_p, buf + x, k - x);
-else
-_strncpy(new_p, buf + x, k - x + 1);
-y += k - x;
-x = k;
-p = new_p;
-if (length)
-*length = y;
-*ptr = p;
-return (y);
+	static char buf[READ_BUF_SIZE];
+	static size_t f, len;
+	size_t v;
+	ssize_t x = 0, y = 0;
+	char *p = NULL, *new_p = NULL, *c;
+
+	p = *ptr;
+	if (p && length)
+		y = *length;
+	if (f == len)
+		f = len = 0;
+
+	x = read_buf(info, buf, &len);
+	if (x == -1 || (x == 0 && len == 0))
+		return (-1);
+
+	c = _strchr(buf + f, '\n');
+	v = c ? 1 + (unsigned int)(c - buf) : len;
+	new_p = _realloc(p, y, y ? y + v : v + 1);
+	if (!new_p) 
+		return (p ? free(p), -1 : -1);
+
+	if (y)
+		_strncat(new_p, buf + f, v - f);
+	else
+		_strncpy(new_p, buf + f, v - f + 1);
+
+	s += v - f;
+	f = v;
+	p = new_p;
+
+	if (length)
+		*length = y;
+	*ptr = p;
+	return (y);
 }
+
 /**
-* sigintHandler - blocks ctrl-C
-* @sig_num: the signal number
-*
-* Return: void
-*/
+ * sigintHandler - blocks ctrl-C
+ * @sig_num: the signal number
+ *
+ * Return: void
+ */
 void sigintHandler(__attribute__((unused))int sig_num)
 {
-_puts("\n");
-_puts("$ ");
-_putchar(BUF_FLUSH);
+	_puts("\n");
+	_puts("$ ");
+	_putchar(BUF_FLUSH);
 }
