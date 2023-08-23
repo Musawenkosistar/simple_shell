b5063be1a5f84fd64c7770356d27578f202972b1
Tshireletso11 <133275866+Tshireletso11@users.noreply.github.com>
2023-08-23 18:44:19 +0200

    Update shell_loop.c

--- shell_loop.c
+++ shell_loop.c
@@ -9,37 +9,38 @@
 */
 int hsh(info_t *info, char **av)
 {
-ssize_t y = 0;
-int builtin_ret = 0;
-while (v!= -1 && builtin_ret != -2)
-{
-clear_info(info);
-if (interactive(info))
-_puts("$ ");
-_eputchar(BUF_FLUSH);
-v = get_input(info);
-if (v!= -1)
-{
-set_info(info, av);
-builtin_ret = find_builtin(info);
-if (builtin_ret == -1)
-find_cmd(info);
-}
-else if (interactive(info))
-_putchar('\n');
-free_info(info, 0);
-}
-write_history(info);
-free_info(info, 1);
-if (!interactive(info) && info->status)
-exit(info->status);
-if (builtin_ret == -2)
-{
-if (info->err_num == -1)
-exit(info->status);
-exit(info->err_num);
-}
-return (builtin_ret);
+	ssize_t v = 0;
+	int builtin_ret = 0;
+
+	while (v != -1 && builtin_ret != -2)
+	{
+		clear_info(info);
+		if (interactive(info))
+			_puts("$ ");
+		_eputchar(BUF_FLUSH);
+		v = get_input(info);
+		if (v != -1)
+		{
+			set_info(info, av);
+			builtin_ret = find_builtin(info);
+			if (builtin_ret == -1)
+				find_cmd(info);
+		}
+		else if (interactive(info))
+			_putchar('\n');
+		free_info(info, 0);
+	}
+	write_history(info);
+	free_info(info, 1);
+	if (!interactive(info) && info->status)
+		exit(info->status);
+	if (builtin_ret == -2)
+	{
+		if (info->err_num == -1)
+			exit(info->status);
+		exit(info->err_num);
+	}
+	return (builtin_ret);
 }
 /**
 * find_builtin - Finds a builtin command
